09cc885f8a7e23da9b3ff3ab5740f848f9e539c9
ffahri <admin@webischia.com>
2017-12-08 13:46:21 +0300

    Updated messageRepository

--- src/main/java/com/webischia/ticketmanagement/Repositories/MessageRepository.java
+++ src/main/java/com/webischia/ticketmanagement/Repositories/MessageRepository.java
@@ -7,7 +7,8 @@ import java.util.Optional;
 
 public interface MessageRepository extends CrudRepository <Message,Long>{
 
-  /*  Optional<Message> findById(long id);
-    Optional<Message> findByClientId(int id);
-    //Optional<Message> findByEmployeeId(int id);*/
+    Optional<Message> findById(long id);
+    Optional<Message> findByUserMessage(int id);
+    Optional<Message> findByTicketMessage(int id);
+    Optional<Message> findByMessageContext(String context); //searching by messagecontext WHERE IS THE PRIVACY ???
 }
