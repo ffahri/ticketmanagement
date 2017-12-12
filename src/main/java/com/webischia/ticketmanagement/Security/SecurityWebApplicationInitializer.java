50654e44945d7426823bdca4becc67a460c252d3
ffahri <admin@webischia.com>
2017-12-12 23:36:37 +0300

    YAYY SPRING SECURITY

---
+++ src/main/java/com/webischia/ticketmanagement/Security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,24 @@
+package com.webischia.ticketmanagement.Security;
+
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {SecurityConfig.class};
+    }
+
+    @Nullable
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
